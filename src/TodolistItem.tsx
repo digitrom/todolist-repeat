4157011f95e6fef0122163ef06e75d1fcfa05731
digitrom <roma.bobrovski@gmail.com>
2025-04-07 12:24:57 +0200

    commit 3

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -4,10 +4,11 @@ import {TaskType} from "./App";
 type TodolistType = {
     title: string
     tasks: Array<TaskType>
+    date?: string
 }
 
 
-export const TodolistItem: FC<TodolistType> = ({title,  tasks}) => {
+export const TodolistItem: FC<TodolistType> = ({title,  tasks, date}) => {
     return (
         <div className="todolist">
             <h1>{title}</h1>
@@ -27,11 +28,12 @@ export const TodolistItem: FC<TodolistType> = ({title,  tasks}) => {
                     <input type="checkbox" checked={true}/>
                     <span>{tasks[2].title}</span>
                 </li>
-            </ul>
+                   </ul>
             <div>
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
+                <div>{date}</div>
             </div>
         </div>
     )
